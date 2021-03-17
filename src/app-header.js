f7ad14abd551a48572ca70392ad305ab08e8e025
marcinsudol <marcin.sudol@outlook.com>
2021-03-17 16:43:35 +0100

    styled add-header, added google font

--- src/app-header.js
+++ src/app-header.js
@@ -1,15 +1,17 @@
 import "./app-header.scss";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function AppHeader() {
   return (
     <nav id="app-header">
-      <Link to="/app/posts">Posts</Link>
-      <Link to="/app/friends">Friends</Link>
-      <Link to="/app/messages">Messages</Link>
-      <Link to="/app/profile">Profile</Link>
-      <Link to="/login">Log out</Link>
+      <NavLink to="/app/posts">Posts</NavLink>
+      <NavLink to="/app/friends">Friends</NavLink>
+      <NavLink to="/app/messages">Messages</NavLink>
+      <NavLink to="/app/profile">Profile</NavLink>
+      <NavLink to="/login" id="logout-button">
+        Log out
+      </NavLink>
     </nav>
   );
 }
