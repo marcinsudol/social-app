e96a4c4d093ace813bdc7da0bc4d774fa596a6a8
marcinsudol <marcin.sudol@outlook.com>
2021-03-18 12:43:33 +0100

    form submition change

--- src/login-form.js
+++ src/login-form.js
@@ -5,20 +5,20 @@ export default function LoginForm() {
   const history = useHistory();
 
   return (
-    <form id="login-form">
+    <form
+      id="login-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        history.push("/app");
+      }}
+    >
       <label htmlFor="login-username">Username</label>
       <input id="login-username" name="username" type="text" />
 
       <label htmlFor="login-password">Password</label>
       <input id="login-password" name="password" type="password" />
 
-      <button
-        onClick={() => {
-          history.push("/app");
-        }}
-      >
-        Log in
-      </button>
+      <button type="submit">Log in</button>
     </form>
   );
 }
