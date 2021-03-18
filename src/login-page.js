08d9ca094753a7aaa568aaac5486bc5d4c13d828
marcinsudol <marcin.sudol@outlook.com>
2021-03-18 21:55:59 +0100

    created and styled login page

--- src/login-page.js
+++ src/login-page.js
@@ -4,7 +4,20 @@ import LoginForm from "./login-form";
 export default function LoginPage() {
   return (
     <div id="login-page">
-      <LoginForm />
+      <header>Social App</header>
+      <div id="login-form-container">
+        <LoginForm />
+      </div>
+      <footer>
+        Photo by{" "}
+        <a href="https://unsplash.com/@marekpiwnicki?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Marek Piwnicki
+        </a>{" "}
+        on{" "}
+        <a href="/t/textures-patterns?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Unsplash
+        </a>
+      </footer>
     </div>
   );
 }
