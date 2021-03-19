67a5576525db79ce3ebf9505bd845cdea0f93e6a
marcinsudol <marcin.sudol@outlook.com>
2021-03-19 11:57:41 +0100

    added icons for header

--- src/app-header.js
+++ src/app-header.js
@@ -1,16 +1,36 @@
 import "./app-header.scss";
 
+import {
+  IoGridOutline,
+  IoPeople,
+  IoChatbubblesOutline,
+  IoPersonCircleOutline,
+  IoLogOutOutline,
+} from "react-icons/io5";
 import { NavLink } from "react-router-dom";
 
 export default function AppHeader() {
   return (
     <nav id="app-header">
-      <NavLink to="/app/posts">Posts</NavLink>
-      <NavLink to="/app/friends">Friends</NavLink>
-      <NavLink to="/app/messages">Messages</NavLink>
-      <NavLink to="/app/profile">Profile</NavLink>
+      <NavLink to="/app/posts">
+        <IoGridOutline />
+        <span>Posts</span>
+      </NavLink>
+      <NavLink to="/app/friends">
+        <IoPeople />
+        <span>Friends</span>
+      </NavLink>
+      <NavLink to="/app/messages">
+        <IoChatbubblesOutline />
+        <span>Messages</span>
+      </NavLink>
+      <NavLink to="/app/profile">
+        <IoPersonCircleOutline />
+        <span>Profile</span>
+      </NavLink>
       <NavLink to="/login" id="logout-button">
-        Log out
+        <IoLogOutOutline />
+        <span>Log out</span>
       </NavLink>
     </nav>
   );
