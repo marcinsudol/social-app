744835b149936bac9b861dfb928668607839b525
marcinsudol <marcin.sudol@outlook.com>
2021-03-26 22:00:33 +0100

    added new message form

--- src/conversation.js
+++ src/conversation.js
@@ -4,17 +4,47 @@ import Message from "./message";
 import { useFetchUser, useFetchMessages } from "./use-fetch";
 import { authContext } from "./auth";
 import "./conversation.scss";
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 
 export default function Conversation({ friendId }) {
+  const [messages, setMessages] = useState(null);
   const auth = useContext(authContext);
   const [user, userError] = useFetchUser(auth.userId);
   const [friend, friendError] = useFetchUser(friendId);
-  const [messages, messagesError] = useFetchMessages(friendId);
+  const [fetchedMessages, messagesError] = useFetchMessages(friendId);
+  const newMessageRef = useRef();
+
+  useEffect(() => {
+    setMessages(fetchedMessages);
+  }, [fetchedMessages]);
+
+  // send message - add to messages list
+  const sendMessage = useCallback(
+    (e) => {
+      e.preventDefault();
+      const newMessageContent = newMessageRef.current.value.trim();
+      if (newMessageContent) {
+        // create new message object
+        const newMessage = {
+          id: new Date().getTime(),
+          userId: auth.userId,
+          friendId,
+          content: newMessageContent,
+          createdAt: new Date().toUTCString(),
+        };
+        newMessageRef.current.value = "";
+        const newMessages = [...messages, newMessage];
+        setMessages(newMessages);
+      }
+    },
+    [auth, friendId, messages]
+  );
+
   const loading =
     (!user && !userError) ||
     (!friend && !friendError) ||
     (!messages && !messagesError);
+
   const error = userError || friendError || messagesError;
 
   return (
@@ -25,14 +55,17 @@ export default function Conversation({ friendId }) {
             <header>
               <h1>{friend.firstName + " " + friend.lastName}</h1>
             </header>
-            {messages.map((message) => (
-              <Message
-                user={user}
-                friend={friend}
-                message={message}
-                key={message.id}
-              />
-            ))}
+            <ol id="messages-list">
+              {messages.map((message) => (
+                <li key={message.id}>
+                  <Message user={user} friend={friend} message={message} />
+                </li>
+              ))}
+            </ol>
+            <form id="send-message-form" onSubmit={sendMessage}>
+              <input type="text" ref={newMessageRef} />
+              <button type="submit">Send</button>
+            </form>
           </div>
         ) : null}
       </LoadableComponent>
